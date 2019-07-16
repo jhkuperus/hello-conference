dd2921ad023260c1c313d9ad60abe91d209eddb1
Jan-Hendrik Kuperus <jhkuperus@yoink.nl>
2019-07-16 17:29:06 +0200

    Adds demo aliases

---
+++ demo.sh
@@ -0,0 +1,10 @@
+
+function git_branch() {
+  local BRANCH=$(git branch --no-color 2> /dev/null | sed -e "/^[^*]/d" -e "s/* \(.*\)/\1/")
+  [[ -n $BRANCH ]] && echo $BRANCH || echo '?'
+}
+
+alias mr="open 'http://localhost/jhkuperus/$(basename `pwd`)/merge_requests/new?merge_request%5Bsource_branch%5D=$(git_branch)'"
+alias ci="open 'http://localhost:8000/job/$(basename `pwd`)'"
+
+export SESSION_DANGER="DEMO"
